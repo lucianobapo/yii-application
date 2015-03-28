321025e72672c3414a407a200e06ed062b53171d
Luciano Porto <luciano.bapo@gmail.com>
2015-03-27 23:35:40 -0300

    27-03-2015-23:35

--- themes/delivery/views/layouts/column3.php
+++ themes/delivery/views/layouts/column3.php
@@ -3,26 +3,32 @@
     $this->beginContent('//layouts/main');
     $this->renderPartial('application.views.site.angularApp');
 ?>
-<section class="main-body" ng-controller="SiteController as site">
 
-    <?php if (Yii::app()->params['boxSocial']): ?>
-    <div class="container-delivery">
-        <div class="box-top">
-            <span class="">Facebook:</span>
-            <span class=""><?php echo Helpers::getFacebookLike('botao2'); ?></span>
+    <section class="ng-show" ng-hide="true">
+        <div class="container-delivery text-center"><i class="fa fa-spinner fa-spin"></i> <?php echo Helpers::t('appUi','Carregando...'); ?></div>
+    </section>
+
+    <section class="main-body ng-hide" ng-controller="SiteController as site" ng-show="true">
+
+        <?php if (Yii::app()->params['boxSocial']): ?>
+            <div class="container-delivery">
+                <div class="box-top">
+                    <span class="">Facebook:</span>
+                    <span class=""><?php echo Helpers::getFacebookLike('botao2'); ?></span>
+                </div>
+                <div class="box-top">
+                    <span class="">Google:</span>
+                    <?php echo Helpers::getGoogleLike('botao2'); ?>
+                </div>
+            </div>
+
+        <?php endif; ?>
+
+        <div class="container-delivery">
+            <div id="content">
+                <?php echo $content; ?>
+            </div><!-- content -->
         </div>
-        <div class="box-top">
-            <span class="">Google:</span>
-            <?php echo Helpers::getGoogleLike('botao2'); ?>
-        </div>
-    </div>
+    </section>
 
-    <?php endif; ?>
-
-    <div class="container-delivery">
-        <div id="content">
-            <?php echo $content; ?>
-        </div><!-- content -->
-	</div>
-</section>
 <?php $this->endContent(); ?>
