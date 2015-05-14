2bf170706c7d45dfc82638cab606516bbc66d8cc
Luciano Porto <luciano.bapo@gmail.com>
2015-05-14 19:23:58 -0300

    facebook

--- themes/delivery/views/layouts/tpl_header.php
+++ themes/delivery/views/layouts/tpl_header.php
@@ -137,6 +137,7 @@
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $baseUrl;?>/img/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="<?php echo $baseUrl;?>/img/ico/apple-touch-icon-57-precomposed.png">
 
+    <?php echo Yii::app()->params['FacebookAnalyticsCode']; ?>
 </head>
 
 <body class="vb21">
