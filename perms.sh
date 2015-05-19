0fdc2ca1475d37e15c75c751451504cb3e96771b
Luciano Porto <luciano.bapo@gmail.com>
2015-05-18 22:46:11 -0300

    fotos

--- perms.sh
+++ perms.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 echo "dia $(date +%Y%m%d)"
-chmod 750 -R /var/www/appyii_v1/
-chgrp www-data -R /var/www/appyii_v1/
-chown luciano -R /var/www/appyii_v1/
-chmod 770 /var/www/appyii_v1/
-chmod 770 /var/www/appyii_v1/images/
-chmod 770 -R /var/www/appyii_v1/assets/
-chmod 770 -R /var/www/appyii_v1/protected/runtime/
-chmod 770 -R /var/www/appyii_v1/.git/
-chmod 770 /var/www/appyii_v1/git-add.sh
-chmod 700 /var/www/appyii_v1/perms.sh
-chmod 700 /var/www/appyii_v1/ftp-perms.sh
-chmod 700 /var/www/appyii_v1/bacen.sh
-chmod 700 /var/www/appyii_v1/mysql_dump.sh
+chmod 750 -R ./
+chgrp www-data -R ./
+chown ubuntu -R ./
+chmod 770 ./
+chmod 770 -R images/
+chmod 770 -R assets/
+chmod 770 -R protected/runtime/
+chmod 700 perms.sh
+chmod 700 ftp-perms.sh
+chmod 700 bacen.sh
+chmod 700 mysql_dump.sh
 
